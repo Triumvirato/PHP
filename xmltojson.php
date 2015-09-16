1a42f60e633409d89261a0dd951c9a6c7e0c3e74
Marco <catajoe@tiscali.it>
2015-09-16 12:27:43 +0200

    class xml to json

--- xmltojson.php
+++ xmltojson.php
@@ -12,12 +12,24 @@ class XmlToJson {
 
 		$simpleXml = simplexml_load_string($fileContents);
 
-		$json = json_encode($simpleXml);
+		$json = json_encode($simpleXml, JSON_PRETTY_PRINT);
 
 		return $json;
 
 	}
 
 }
+
+$xmltojson = new XmlToJson();
+
+$jsonobj = $xmltojson->Parse('https://bugs.documentfoundation.org/show_bug.cgi?ctype=xml&id=71409');
+
+header('Content-Type: application/json');
+
+echo $jsonobj;
+
+
+
+
 
 ?>
