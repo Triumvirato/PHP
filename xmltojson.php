5b826ab6cbbc711dde02fa0f0d6609224991c5bd
Marco <catajoe@tiscali.it>
2015-09-16 12:35:56 +0200

    class xml to json

--- xmltojson.php
+++ xmltojson.php
@@ -9,10 +9,15 @@ class XmlToJson {
 		$fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
 
 		$fileContents = trim(str_replace('"', "'", $fileContents));
+		
+	    //$fileContents = str_replace('\/', '/', $fileContents); //bob fix
+
+		//echo $fileContents;
 
 		$simpleXml = simplexml_load_string($fileContents);
 
 		$json = json_encode($simpleXml, JSON_PRETTY_PRINT);
+	
 
 		return $json;
 
@@ -22,11 +27,13 @@ class XmlToJson {
 
 $xmltojson = new XmlToJson();
 
-$jsonobj = $xmltojson->Parse('https://bugs.documentfoundation.org/show_bug.cgi?ctype=xml&id=71409');
+
+$js = $xmltojson->Parse('https://bugs.documentfoundation.org/show_bug.cgi?ctype=xml&id=71409');
+
 
 header('Content-Type: application/json');
 
-echo $jsonobj;
+echo $js;
 
 
 
