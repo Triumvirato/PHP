cef55d2dc779f475b7b4cf995f13b7836ff7bb8e
roberto2991 <robertomasellis@gmail.com>
2015-09-18 14:07:35 +0200

    new

--- finale/funzioni.php
+++ finale/funzioni.php
@@ -95,9 +95,14 @@ function getBugs($id,$con)
 		
 
 		$simpleXml = simplexml_load_string($fileContents);
+    
+        unset($simpleXml->bug->attachment);
+    
 
 		$json = json_encode($simpleXml);//JSON_PRETTY_PRINT
-        $json = json_decode($json);
+        
+    
+    $json = json_decode($json);
     
     
     
@@ -121,5 +126,55 @@ function getBugs($id,$con)
     
 }
 
+function getFromRpc($url, $xml)
+{
+
+  $ch = curl_init();
+  curl_setopt( $ch, CURLOPT_URL, $url );
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt( $ch, CURLOPT_POST, true );
+  curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+  curl_setopt( $ch, CURLOPT_POSTFIELDS, $xml );
+
+  $result = curl_exec($ch);
+
+  if(!curl_exec($ch)){
+    die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
+}
+
+   //Stampa i risultati in XML
+   //header("Content-type: text/xml");
+   //print $result;
+
+  curl_close($ch);
+    
+    return $result;
+}
+
+function setXML($id)
+{
+    return '<?xml version=\'1.0\' encoding=\'UTF-8\'?><methodCall><methodName>Bug.history</methodName> <params> <param>
+    <struct>      
+        <member>
+            <name>ids</name>
+            <value>
+                <array>
+                    <data>
+                        <value>
+                            <int>'.$id.'</int>
+                        </value>
+                    </data>
+                </array>
+            </value>
+        </member>   
+    </struct>
+</param> </params> </methodCall>';
+        
+}
+
+
+
+?>
 
 
