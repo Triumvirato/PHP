e3789ac0bbc646d63534d8b76aeb8caff32a9b76
Marco <catajoe@tiscali.it>
2015-09-30 11:06:46 +0200

    separazione del front-end e back-end

---
+++ back-end/test.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/funzioni.php';
+
+// MongoDB Connect
+$con=connectToMongo(); 
+
+//Select all CLOSED bug(s)
+$bugzilla_query = $_POST['urlsend'];
+
+// cache in a local cache folder if possible
+$csv = cacheUrl($bugzilla_query . '&ctype=csv');
+
+$conta=0;
+
+
+
+foreach (getBugsFromCSV($csv) as $bug_number => $bug_title){
+
+    if (!empty($bug_number)) { 
+        getBugs($bug_number, $con);     
+    }
+	
+	$conta++;
+
+	if ($conta == 5){
+
+    break;
+	}
+		
+	
+}
+
+
+
+?>
